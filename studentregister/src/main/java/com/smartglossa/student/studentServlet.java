f317715092060fbcf5b6faf0bfa5c8ec8d4c88a2
deepikabu <deepika@smartglossa.com>
2016-12-01 11:31:29 -0800

    add file

--- studentregister/src/main/java/com/smartglossa/student/studentServlet.java
+++ studentregister/src/main/java/com/smartglossa/student/studentServlet.java
@@ -3,6 +3,8 @@ package com.smartglossa.student;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.servlet.ServletException;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class studentServlet extends HttpServlet {
@@ -27,40 +30,52 @@ public class studentServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		JSONObject object = new JSONObject();
+	     JSONObject  object= new JSONObject();
 		String operation = request.getParameter("operation");
 		if (operation.equals("register")) {
-			int RollNo = Integer.parseInt(request.getParameter("RollNo"));
-			String Name = request.getParameter("Name");
-			int Age = Integer.parseInt(request.getParameter("Age"));
+		
+			int sId = Integer.parseInt(request.getParameter("sId"));
+			String studentName = request.getParameter("studentName");
 			String Gender = request.getParameter("Gender");
-			String MotherName = request.getParameter("MotherName");
-			String FatherName = request.getParameter("FatherName");
 			String Address = request.getParameter("Address");
-			int MobileNumber = Integer.parseInt(request.getParameter("MobileNumber"));
-			String BloodGroup = request.getParameter("BloodGroup");
-			int JoinDate = Integer.parseInt(request.getParameter("JoinDate"));
-			String JoinClass = request.getParameter("JoinClass");
-			int Dob = Integer.parseInt(request.getParameter("DOB"));
+			String contactNumber = request.getParameter("contactNumber");
+			String joinDate = request.getParameter("joinDate");
+			String Dob = request.getParameter("DOB");
+			
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/student", "root", "root");
+					Statement statement = conn.createStatement();
+					String query = "insert into studentdetails(sId,studentName,Address,contactNumber,Gender,joinDate,DOB)values("
+							+ sId + ",'" + studentName + "','" + Gender + "','" + Address + "','" + contactNumber + "','" + joinDate + "','" + Dob+"')";
+					statement.execute(query);
+					object.put("status", "1");
+				} catch (Exception e) {
+                  		
+					object.put("status", "0");
+					e.printStackTrace();
+				}
+ 
+				response.getWriter().println(object);
 
+			
+
+		}
+		else if(operation.equals("getAll")){
+			JSONArray set = new JSONArray();
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
 				Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/student", "root", "root");
 				Statement statement = conn.createStatement();
-				String query = "insert into studentdetails(RollNo,Name,Age,Gender,MotherName,FatherName,Address,MobileNumber,BloodGroup,JoinDate,JoinClass,DOB)values("
-						+ RollNo + ",'" + Name + "'," + Age + ",'" + Gender + "','" + MotherName + "','" + FatherName
-						+ "','" + Address + "'," + MobileNumber + ",'" + BloodGroup + "'," + JoinDate + ",'" + JoinClass
-						+ "'," + Dob + ")";
-				statement.execute(query);
-				object.put("status", "1");
+				String query = "select * from studentdetails";
+				ResultSet result = statement.executeQuery(query);
 			} catch (Exception e) {
-
+				// TODO Auto-generated catch block
 				e.printStackTrace();
-
 			}
-			response.getWriter().println(object);
-
+			
+			
 		}
-
 	}
 }
+
